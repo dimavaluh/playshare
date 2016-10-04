6bf1fa8f442e7cc4c185539994cea65fac6a945d
shonie <starnikov2009@ukr.net>
2016-10-04 23:08:57 +0300

    Added handler if the username or email already exists in the database

--- public/app/components/login/login.controller.js
+++ public/app/components/login/login.controller.js
@@ -1,6 +1,14 @@
 app.controller('loginCtrl', ['$scope', '$http', function($scope, $http){
     $scope.sendData = function(nick, email, pass){
         var data = {'nickName': nick, 'email': email, 'password': pass};
-        $http.post('/api/login', data);
+        $http.post('/api/login', data)
+            .then(function successCallback(response) {
+                // here we must redirect user to the homepage
+            if (response.status == 200) console.log('Successfully created an account');
+        }, function errorCallback(response) {
+                // here we get message that nickName or email already exist, so we have to render this message in the view
+            console.log(response.data); //
+
+        });
     };
 }]);
