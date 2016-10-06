86acfcfa5111a1c5577b0bbfa9dba548ef66b05c
Aleksandr Poleliuk <cawkabss@gmail.com>
2016-10-06 22:42:50 +0300

    Add user account

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -19,18 +19,40 @@ app.controller('mainCtrl',['$scope', 'AppServices', '$location', function($scope
 
     $scope.users = AppServices.getAllUsers();
 
+    $scope.$watch(function(){
+        return AppServices.getUser('signUser');
+    }, function(newCodes, oldCodes){
+        $scope.signUser = JSON.parse(AppServices.getUser());
+    });
+    
+    $scope.login = function(email, pass){
+        AppServices.login(email, pass)
+            .then(function successCallback(response) {
+                // here we must redirect user to the homepage
+                if (response.status == 200){
+                    AppServices.saveSession(response.data);
+                    $scope.signUser = JSON.parse(AppServices.getUser());
+                    $location.path('#')
+                }
+            }, function errorCallback(response) {
+                // here we get message that nickName or email already exist, so we have to render this message in the view
+                console.log(response.data); //
+
+            });
+    };
+
     $scope.logout = function(signUser){
         var nickName = signUser.nickName;
         AppServices.logout(nickName)
                 .then(function successCallback(response) {
                     // here we must redirect user to the homepage
                     if (response.status == 200){
+                        localStorage.clear();
                         $location.path('#')
                     }
                 }, function errorCallback(response) {
                     // here we get message that nickName or email already exist, so we have to render this message in the view
                     console.log(response.data); //
-
                 });
         };
 
